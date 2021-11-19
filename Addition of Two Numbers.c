92d08247563083c5364ea05dbe1ecf962b0f8531
VinayKumar99442 <94681139+VinayKumar99442@users.noreply.github.com>
2021-11-19 15:48:13 +0530

    Create Addition of Two Numbers.c
    Adding the two Variables

---
+++ Addition of Two Numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int argc, char **argv){
+  int a=0,b=0;
+  printf("Enter any 2 Numbers: \n");
+  scanf("%d%d",&a,&b);
+  int c= a+b;
+  printf("Sum Valude: %d\n",c);
+  
+}
